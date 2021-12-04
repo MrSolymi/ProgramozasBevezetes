4c424f7555d8604b9f4dd66054836af0a2e4d532
Solymi <71895616+MrSolymi@users.noreply.github.com>
2021-12-04 15:22:30 +0100

    Add files via upload

--- ch17_drill_v1.cpp
+++ ch17_drill_v1.cpp
@@ -16,6 +16,14 @@ void print_array(ostream& os, int* a, int n)
     }
 }
 
+void print_vector(ostream& os, vector<int> a)
+{
+    for(int i = 0; i < a.size(); i++)
+    {
+        os << a[i] << endl;
+    }
+}
+
 int main(){
     int* a = new int[10];
     for (int i = 0; i < 10; i++)
@@ -51,6 +59,30 @@ int main(){
     print_array(cout, a4, 20);
     cout << endl;
     delete[] a4;
-    
+//-----------------------------------------
+    vector<int> a5;
+    for (int i = 0; i < 10; i++)
+    {
+        a5.push_back(100 + i);
+    }
+    print_vector(cout, a5);
+    cout << endl;
+
+    vector<int> a6;
+    for (int i = 0; i < 11; i++)
+    {
+        a6.push_back(100 + i);
+    }
+    print_vector(cout, a6);
+    cout << endl;
+
+    vector<int> a7;
+    for (int i = 0; i < 20; i++)
+    {
+        a7.push_back(100 + i);
+    }
+    print_vector(cout, a7);
+    cout << endl;
+
     return 0;
 }
